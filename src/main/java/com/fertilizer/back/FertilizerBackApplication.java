711583d7b364117688bc18abdd7dd477a12d04af
bjune-park <54969248+bj11077@users.noreply.github.com>
2022-01-26 07:28:54 +0900

    스케줄거 허용 어노테이션 추가

--- src/main/java/com/fertilizer/back/FertilizerBackApplication.java
+++ src/main/java/com/fertilizer/back/FertilizerBackApplication.java
@@ -1,10 +1,16 @@
 package com.fertilizer.back;
 
+import com.fertilizer.back.service.kafkaExam.consumer.UserTopicConsumer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FertilizerBackApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(FertilizerBackApplication.class, args);
